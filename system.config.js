802518b3d0046899316c46655cf15d7e52ca1b7c
Edward Opare-Aryee <ed@thisissoon.com>
2016-03-07 11:15:06 +0000

    get tests working

--- system.config.js
+++ system.config.js
@@ -1,6 +1,10 @@
 System.config({
   defaultJSExtensions: true,
   map: {
+    'systemjs': 'node_modules/systemjs/dist/system.js',
+    'system-polyfills': 'node_modules/systemjs/dist/system-polyfills.js',
+    'es6-module-loader': 'node_modules/es6-module-loader/dist/es6-module-loader.js',
+    'phantomjs-polyfill': 'node_modules/phantomjs-polyfill/bind-polyfill.js',
     'typescript': 'node_modules/typescript/lib/typescript.js',
     'angular2': 'node_modules/angular2',
     'angular2-universal-preview': 'node_modules/angular2-universal-preview/dist/browser/index',
