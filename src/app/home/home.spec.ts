9add1af5bab7d8eff64546621721fe6ef1419494
Edward Opare-Aryee <ed@thisissoon.com>
2016-11-05 19:15:34 +0000

    fix tests

--- src/app/home/home.spec.ts
+++ src/app/home/home.spec.ts
@@ -1,56 +1,34 @@
-import {
-  inject,
-  TestBed
-} from '@angular/core/testing';
-import { Component } from '@angular/core';
-import {
-  BaseRequestOptions,
-  ConnectionBackend,
-  Http
-} from '@angular/http';
-import { MockBackend } from '@angular/http/testing';
+import { inject, TestBed } from '@angular/core/testing';
+
 
 // Load the implementations that should be tested
 import { HomeComponent } from './home.component';
 import { Todo } from './todo.model';
-import { StoreService } from '../store';
+import { StoreService } from '../store/store.service';
 
+let todos: Todo[] = [
+  { title: 'task1', done: true },
+  { title: 'task2', done: false },
+  { title: 'task3', done: false }
+];
 describe('HomeComponent', () => {
-  let todos: Todo[] = [
-    { title: 'task1', done: true },
-    { title: 'task2', done: false },
-    { title: 'task3', done: false }
-  ];
   // provide our implementations or mocks to the dependency injector
   beforeEach(() => TestBed.configureTestingModule({
     providers: [
-      BaseRequestOptions,
-      MockBackend,
-      {
-        provide: Http,
-        useFactory: function(backend: ConnectionBackend, defaultOptions: BaseRequestOptions) {
-          return new Http(backend, defaultOptions);
-        },
-        deps: [MockBackend, BaseRequestOptions]
-      },
-      {
-        provide: StoreService,
-        useClass: class {
-          get(key: string) { return todos; }
-          set(key: string) { }
-        }
-      },
+      StoreService,
       HomeComponent
     ]
   }));
 
-  it('should have default data', inject([ HomeComponent ], (home: HomeComponent) => {
+  it('should have default data', inject([ HomeComponent, StoreService ], (home: HomeComponent, store: StoreService) => {
     expect(home._todos).toEqual([]);
     expect(home.title).toEqual('');
   }));
 
-  it('should log ngOnInit', inject([ HomeComponent ], (home: HomeComponent) => {
-    let spy = spyOn(home.store, 'get').and.callThrough();
+  it('should get todos from store', inject([ HomeComponent, StoreService ], (home: HomeComponent, store: StoreService) => {
+    let spy = spyOn(home.store, 'get').and.callFake(() => {
+      return todos;
+    });
     expect(spy).not.toHaveBeenCalled();
 
     home.ngOnInit();
